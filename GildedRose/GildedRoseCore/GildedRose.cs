bfec897a85ab08a1b5f0da3817c99faa362b4c8b
Alejandro Jimenez <ajimenez@decentraland.org>
2022-11-04 16:24:25 +0100

    chore: reformat

--- GildedRose/GildedRoseCore/GildedRose.cs
+++ GildedRose/GildedRoseCore/GildedRose.cs
@@ -2,27 +2,40 @@ namespace GildedRoseCore;
 
 public class GildedRose
 {
-    public static void UpdateQuality(List<Item> items) {
-        for (int i = 0; i < items.Count; i++) {
-            if ((!"Aged Brie".Equals(items[i].GetName())) && !"Backstage passes to a TAFKAL80ETC concert".Equals(items[i].GetName())) {
-                if (items[i].GetQuality() > 0) {
-                    if (!"Sulfuras, Hand of Ragnaros".Equals(items[i].GetName())) {
+    public static void UpdateQuality(List<Item> items)
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+            if ((!"Aged Brie".Equals(items[i].GetName())) && !"Backstage passes to a TAFKAL80ETC concert".Equals(items[i].GetName()))
+            {
+                if (items[i].GetQuality() > 0)
+                {
+                    if (!"Sulfuras, Hand of Ragnaros".Equals(items[i].GetName()))
+                    {
                         items[i].SetQuality(items[i].GetQuality() - 1);
                     }
                 }
-            } else {
-                if (items[i].GetQuality() < 50) {
+            }
+            else
+            {
+                if (items[i].GetQuality() < 50)
+                {
                     items[i].SetQuality(items[i].GetQuality() + 1);
 
-                    if ("Backstage passes to a TAFKAL80ETC concert".Equals(items[i].GetName())) {
-                        if (items[i].GetSellIn() < 11) {
-                            if (items[i].GetQuality() < 50) {
+                    if ("Backstage passes to a TAFKAL80ETC concert".Equals(items[i].GetName()))
+                    {
+                        if (items[i].GetSellIn() < 11)
+                        {
+                            if (items[i].GetQuality() < 50)
+                            {
                                 items[i].SetQuality(items[i].GetQuality() + 1);
                             }
                         }
 
-                        if (items[i].GetSellIn() < 6) {
-                            if (items[i].GetQuality() < 50) {
+                        if (items[i].GetSellIn() < 6)
+                        {
+                            if (items[i].GetQuality() < 50)
+                            {
                                 items[i].SetQuality(items[i].GetQuality() + 1);
                             }
                         }
@@ -30,23 +43,34 @@ public class GildedRose
                 }
             }
 
-            if (!"Sulfuras, Hand of Ragnaros".Equals(items[i].GetName())) {
+            if (!"Sulfuras, Hand of Ragnaros".Equals(items[i].GetName()))
+            {
                 items[i].SetSellIn(items[i].GetSellIn() - 1);
             }
 
-            if (items[i].GetSellIn() < 0) {
-                if (!"Aged Brie".Equals(items[i].GetName())) {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".Equals(items[i].GetName())) {
-                        if (items[i].GetQuality() > 0) {
-                            if (!"Sulfuras, Hand of Ragnaros".Equals(items[i].GetName())) {
+            if (items[i].GetSellIn() < 0)
+            {
+                if (!"Aged Brie".Equals(items[i].GetName()))
+                {
+                    if (!"Backstage passes to a TAFKAL80ETC concert".Equals(items[i].GetName()))
+                    {
+                        if (items[i].GetQuality() > 0)
+                        {
+                            if (!"Sulfuras, Hand of Ragnaros".Equals(items[i].GetName()))
+                            {
                                 items[i].SetQuality(items[i].GetQuality() - 1);
                             }
                         }
-                    } else {
+                    }
+                    else
+                    {
                         items[i].SetQuality(items[i].GetQuality() - items[i].GetQuality());
                     }
-                } else {
-                    if (items[i].GetQuality() < 50) {
+                }
+                else
+                {
+                    if (items[i].GetQuality() < 50)
+                    {
                         items[i].SetQuality(items[i].GetQuality() + 1);
                     }
                 }
